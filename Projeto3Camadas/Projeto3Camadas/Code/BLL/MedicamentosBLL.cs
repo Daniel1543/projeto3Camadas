fdd38013c5cf772565d24845278958ab4dfc8e1c
DANIEL_DE_MELO_SOARES <11900342@DELTAB06.BARROCA>
2021-10-28 21:43:46 -0300

    Projeto

---
+++ Projeto3Camadas/Projeto3Camadas/Code/BLL/MedicamentosBLL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto3Camadas.Code.DTO;
+
+namespace Projeto3Camadas.Code.BLL
+{
+    class MedicamentosBLL
+    {
+
+
+        public void Inserir(MedicamentosDTO meddto)
+        {
+
+        }
+    }
+}
---
+++ Projeto3Camadas/Projeto3Camadas/Ui/Frm_Medicamentos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+using Projeto3Camadas.Code.BLL;
+using Projeto3Camadas.Code.DTO;
+
+namespace Projeto3Camadas.Ui
+{
+    public partial class Frm_Medicamentos : Form
+    {
+        
+        MedicamentosBLL medbll = new MedicamentosBLL();
+        MedicamentosDTO meddto = new MedicamentosDTO();
+
+        public Frm_Medicamentos()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            meddto.Medicamento = txtMedicamentos.Text;
+            meddto.Composicao = txtComposicao.Text;
+
+            medbll.Inserir(meddto);
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
