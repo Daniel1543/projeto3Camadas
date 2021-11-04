95323443ab05e1cf93ada086b4f8b1ec74abeaba
Daniel <89932146+Daniel1543@users.noreply.github.com>
2021-11-04 16:01:10 -0300

    Add files via upload

--- Projeto3Camadas/Projeto3Camadas/Code/BLL/MedicamentosBLL.cs
+++ Projeto3Camadas/Projeto3Camadas/Code/BLL/MedicamentosBLL.cs
@@ -4,14 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Projeto3Camadas.Code.DTO;
+using Projeto3Camadas.Code.DAL;
 
 namespace Projeto3Camadas.Code.BLL
 {
     class MedicamentosBLL
     {
 
+        Conexao conexao = new Conexao();
+        string tabela = "tbl_medicamentos";
+
+
 
         public void Inserir(MedicamentosDTO meddto)
+        {
+            string inserir = $"insert into {tabela} values (null, '{meddto.Medicamento}', '{meddto.Composicao}')";
+            conexao.ExecutarComando(inserir);
+        }
+
+        public void Editar(MedicamentosDTO meddto)
+        {
+
+        }
+
+        public void Excluir(MedicamentosDTO meddto)
+        {
+
+        }
+
+        public void listar()
         {
 
         }
